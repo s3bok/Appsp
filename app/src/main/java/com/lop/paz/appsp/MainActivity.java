9938c75e8a19498bf88ffb0fe4406ed62df61344
s3bok <paznogard@gmail.com>
2016-06-09 00:13:03 -0500

    Subiendo cambios de layout en lista

--- app/src/main/java/com/lop/paz/appsp/MainActivity.java
+++ app/src/main/java/com/lop/paz/appsp/MainActivity.java
@@ -12,34 +12,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mListView = (ListView) findViewById(R.id.avisos_list_view);
+        //El arrayAdapter es el controller en model-view-controller
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                //Context
+                this,
+                //layout (view)
+                R.layout.avisos_row,
+                //campo qe muestra los datos
+                R.id.row_text,
+                // (model) modelo de datos para probar lista
+                new String[]{"Prueba Uno", "Prueba Dos", "prueba Tres"});
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        mListView.setAdapter(arrayAdapter);
     }
 
     @Override
