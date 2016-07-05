745b0c48b6e60a9e2a36de5d38e46ddb637d752a
s3bok <paznogard@gmail.com>
2016-07-05 00:09:54 -0500

    crud terminado

--- app/src/main/java/com/lop/paz/appsp/MainActivity.java
+++ app/src/main/java/com/lop/paz/appsp/MainActivity.java
@@ -20,11 +20,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.AbsListView;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
@@ -107,12 +113,13 @@ public class MainActivity extends AppCompatActivity
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         //editar aviso
                         if (position == 0) {
-                            Toast.makeText(MainActivity.this, "editar "
-                                    + masterListPosition, Toast.LENGTH_SHORT).show();
+                            int nId = getIdFromPosition(masterListPosition);
+                            Aviso aviso = mDBAdapter.fetchReminderById(nId);
+                            fireCustomDialog(aviso);
                             //borrar aviso
                         } else {
-                            Toast.makeText(MainActivity.this, "borrar "
-                                    + masterListPosition, Toast.LENGTH_SHORT).show();
+                            mDBAdapter.deleteReminderById(getIdFromPosition(masterListPosition));
+                            mCursorAdapter.changeCursor(mDBAdapter.fetchAllReminders());
                         }
                         dialog.dismiss();
                     }
@@ -186,12 +193,61 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    private void fireCustomDialog(final Aviso aviso){
+        // custom dialog
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_custom);
+
+        TextView titleView = (TextView) dialog.findViewById(R.id.custom_title);
+        final EditText editCustom = (EditText) dialog.findViewById(R.id.custom_edit_reminder);
+        Button commitButton = (Button) dialog.findViewById(R.id.custom_button_commit);
+        final CheckBox checkBox = (CheckBox) dialog.findViewById(R.id.custom_check_box);
+        LinearLayout rootLayout = (LinearLayout) dialog.findViewById(R.id.custom_root_layout);
+        final boolean isEditOperation = (aviso != null);
+
+        //esto es para un edit
+        if (isEditOperation){
+            titleView.setText("Editar Aviso");
+            checkBox.setChecked(aviso.getmImportant() == 1);
+            editCustom.setText(aviso.getmContet());
+            rootLayout.setBackgroundColor(getResources().getColor(R.color.azul_neutro));
+        }
+
+        commitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String reminderText = editCustom.getText().toString();
+                if (isEditOperation) {
+                    Aviso reminderEdited = new Aviso(aviso.getmId(),
+                            reminderText, checkBox.isChecked() ? 1 : 0);
+                    mDBAdapter.updateReminder(reminderEdited);
+                    //esto es para nuevo aviso
+                } else {
+                    mDBAdapter.createReminder(reminderText, checkBox.isChecked());
+                }
+                mCursorAdapter.changeCursor(mDBAdapter.fetchAllReminders());
+                dialog.dismiss();
+            }
+        });
+
+        Button buttonCancel = (Button) dialog.findViewById(R.id.custom_button_cancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.action_nuevo:
                 //crea nuevo aviso
-                Log.d(getLocalClassName(), "Crear  nuevo aviso");
+                fireCustomDialog(null);
                 return true;
             case R.id.action_salir:
                 finish();
